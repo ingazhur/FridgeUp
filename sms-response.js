1ccfaa0a3a3c376a1298f60f44ad071cf502ee9d
jxwang3 <jxwang3@illinois.edu>
2021-01-16 19:03:22 -0600

    securely stored twilio info

--- sms-response.js
+++ sms-response.js
@@ -1,12 +1,11 @@
-var accountSid = 'AC9b616397831e57f06ee0f6eec20eb791'; // Your Account SID from www.twilio.com/console
-var authToken = '525ff2ca841b0f1a4c1b8ca88d34d394';   // Your Auth Token from www.twilio.com/console
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
     body: 'Hello from Node',
-    to: '+18479753586',  // Text this number
+    to: '+18473933309',  // Text this number
     from: '+12187369065' // From a valid Twilio number
 })
 .then((message) => console.log(message.sid));
